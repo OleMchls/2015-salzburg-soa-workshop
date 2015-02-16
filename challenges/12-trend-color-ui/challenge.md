fa5222f5a523ebc6a59cb216389625ec73db03e4
Ole Michaelis <Ole.Michaelis@googlemail.com>
2015-02-17 00:41:50 +0100

    add challenge 12

---
+++ challenges/12-trend-color-ui/challenge.md
@@ -0,0 +1,31 @@
+# Challenge 12 - Trend Color UI
+
+Now it's your task to write a web layer, which displays the data from the API. In order to separate the application logic from the display logic.
+
+Something like this.
+
+![](http://cl.ly/image/47141x2D1I0K/Screen%20Shot%202015-02-17%20at%2000.23.21.png)
+
+But of course it's totally up to you how it looks like. Check the "Test / Compliance" section to make sure your code is complient.
+
+# Setup
+
+```sh
+$ bundle install
+```
+
+# Development
+
+Change the code in `spec/trend_color_ui_spec.rb` line `11` to return your Rack based application and solve the challenge.
+
+You can use whatever rack-based framework you like. And whatever folder structure you like.
+
+# Test / Compliance
+
+Run
+
+```sh
+$ bundle exec rspec spec
+```
+
+to check if your app complies with all the challenge's rules. Yay! :tada:
---
+++ challenges/12-trend-color-ui/spec/trend_color_ui_spec.rb
@@ -0,0 +1,50 @@
+ENV['RACK_ENV'] = 'test'
+
+require 'json'
+require 'rspec'
+require 'rack/test'
+
+describe 'Trend Color API' do
+  include Rack::Test::Methods
+
+  def app
+    ### STUDENTS: make this method return your Rack-based app
+  end
+
+  it 'should have all trend colors' do
+    get '/'
+
+    expect(last_response).to be_ok
+
+    expect(last_response.body).to include '<!DOCTYPE html>'
+
+    expect(last_response.body).to include 'data-color-name="Tangerine"'
+    expect(last_response.body).to include 'data-color-value="#F79256"'
+    expect(last_response.body).to include 'data-color-name="Toasted Almond"'
+    expect(last_response.body).to include 'data-color-value="#D2B29A"'
+    expect(last_response.body).to include 'data-color-name="Aquamarine"'
+    expect(last_response.body).to include 'data-color-value="#9DC6D8"'
+  end
+
+  it 'should have only colors from queried year' do
+    get '/', from: 2015, to: 2015
+
+    expect(last_response.body).not_to include 'data-color-name="Tangerine"'
+    expect(last_response.body).not_to include 'data-color-value="#F79256"'
+    expect(last_response.body).not_to include 'data-color-name="Toasted Almond"'
+    expect(last_response.body).not_to include 'data-color-value="#D2B29A"'
+    expect(last_response.body).to include 'data-color-name="Aquamarine"'
+    expect(last_response.body).to include 'data-color-value="#9DC6D8"'
+  end
+
+  it 'should have only colors from queried year' do
+    get '/', from: 2014, to: 2015
+
+    expect(last_response.body).not_to include 'data-color-name="Tangerine"'
+    expect(last_response.body).not_to include 'data-color-value="#F79256"'
+    expect(last_response.body).to include 'data-color-name="Toasted Almond"'
+    expect(last_response.body).to include 'data-color-value="#D2B29A"'
+    expect(last_response.body).to include 'data-color-name="Aquamarine"'
+    expect(last_response.body).to include 'data-color-value="#9DC6D8"'
+  end
+end
