cfa6ea9f77e0dae4831347435022f625400de8eb
Thorben Schröder <stillepost@gmail.com>
2015-02-21 19:06:19 +0100

    Enables IRB history in the box

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,3 +16,11 @@ sudo unzip ngrok.zip -d /usr/local/bin
 
 install 'httpie' httpie
 install 'nmap' nmap
+
+IRBRC_FILE="/home/vagrant/.irbrc"
+if [ ! -f $IRBRC_FILE ]; then
+  echo "Enabling up IRB history"
+  echo "require 'irb/ext/save-history'" > $IRBRC_FILE
+  echo "IRB.conf[:SAVE_HISTORY] = 100" >> $IRBRC_FILE
+  echo "IRB.conf[:HISTORY_FILE] = File.join(ENV['HOME'], '.irb-save-history')" >> $IRBRC_FILE
+fi
