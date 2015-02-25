b10994341b1f1bf6d7209240847777dfe144f152
Thorben Schröder <stillepost@gmail.com>
2015-02-26 00:05:32 +0100

    Adds a framework for challenge 11's API

---
+++ challenges/11-trend-colors-service/lib/trend_colors_api.rb
@@ -0,0 +1,22 @@
+require 'grape'
+
+class TrendColorsApi < Grape::API
+  TREND_COLORS = {
+    2013 => [
+      { name: 'Tangerine', hex: '#F79256' },
+      { name: 'Custard', hex: '#EAD98B' },
+      { name: 'Marsala', hex: '#955251' },
+    ],
+    2014 => [
+      { name: 'Classic Blue', hex: '#1D4E89' },
+      { name: 'Toasted Almond', hex: '#D2B29A' },
+      { name: 'Strawberry Ice', hex: '#E3868F' },
+    ],
+    2015 => [
+      { name: 'Aquamarine', hex: '#9DC6D8' },
+      { name: 'Scuba Blue', hex: '#00B2CA' },
+      { name: 'Lucite® Green', hex: '#7DCFB6' },
+    ]
+  }
+
+end
